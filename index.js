68d33a4b530107a6fcc2192b3ad6fcdf408ab464
zulhijaya18 <32.zulhijaya@gmail.com>
2023-06-29 00:48:34 +0800

    refactor to pdf

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const http = require('http');
+const http = require("http");
+var pdf = require("pdf-creator-node");
+var fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('nama saya zulhijaya\nsaya belum menikah');
-  res.end();
+  // Read HTML Template
+  var html = fs.readFileSync("template.html", "utf8");
+
+  // Create PDF document
+  var document = {
+    html: html,
+    path: "./output.pdf",
+    data: {
+      users: [
+        {
+          name: "Zul",
+          age: "25",
+        },
+      ],
+    },
+  };
+
+  let options = {
+    format: "A4",
+    orientation: "portrait",
+    border: "10mm",
+  };
+
+  pdf.create(document, options).then((result) => {
+    console.log(result);
+    res.writeHead(200, { "Content-Type": "application/pdf" });
+    var filestream = fs.createReadStream("./output.pdf");
+    filestream.pipe(res);
+  });
 });
 
 server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log("Server listening on port 3000");
 });
